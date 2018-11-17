dd9d07ed946d1197b782b9636d44431eaed61ee1
jdmedlock <jdmedlock@gmail.com>
2018-11-17 11:03:47 -0600

    Feature: Add Splash page
    Add Splash page
    - Replace App.js with one customized for our demonstration
    - Add Splash stateless component
    
    Resolves: N/a
    See also: N/a

---
+++ src/components/Splash.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Splash = (props) => {
+
+  /**
+   * @description Stateless component to generate the Splash page
+   * @returns {HTMLDivElement} Splash page
+   * @memberof Splash
+   */
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+      <p>
+        This is our awesome, although unadorned, app splash page. We'll make
+        this fancy at some point, but at this point it's merely a placeholder.
+      </p>
+      <img src={props.logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+Splash.propTypes = {
+  logo: PropTypes.object.isRequired
+}
+
+export default Splash;
