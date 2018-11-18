8beb3d757e5a06e08fbc0584ea68035ac725ed78
jdmedlock <jdmedlock@gmail.com>
2018-11-17 18:52:11 -0600

    Updates

---
+++ precache-manifest.d112cd38d7b4bd2c5fadfc41828bf10c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0a8f874f4d9bbab7eea2",
+    "url": "/ghpages-react/static/js/runtime~main.0a8f874f.js"
+  },
+  {
+    "revision": "c1628684a18bcb9868af",
+    "url": "/ghpages-react/static/js/main.c1628684.chunk.js"
+  },
+  {
+    "revision": "9efb755083a6801f2b7d",
+    "url": "/ghpages-react/static/js/1.9efb7550.chunk.js"
+  },
+  {
+    "revision": "c1628684a18bcb9868af",
+    "url": "/ghpages-react/static/css/main.9bee8e60.chunk.css"
+  },
+  {
+    "revision": "f02a1d1f918701a5bc68b2b7253e7b04",
+    "url": "/ghpages-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ghpages-react/precache-manifest.9a037b259d679be827afcce48107e2fa.js"
+  "/ghpages-react/precache-manifest.d112cd38d7b4bd2c5fadfc41828bf10c.js"
 );
 
 workbox.clientsClaim();
